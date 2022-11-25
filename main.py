a13a21e34d0edf47f5fb17ea196e8966908cd793
Mariana Galvão <mariana.giannico@hotmail.com>
2022-11-25 16:31:42 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# ENUNCIADO DA QUESTÃO 3
+#Escreva um programa que calcule o preço a pagar pelo fornecimento de energia elétrica. Pergunte a quantidade de kWh consumida e o tipo de instalação: R para residências, I para indústrias e C para comércios
+# Calcule o preço de acordo com a tabela a # seguir:
+# Preço por tipo e faixa de consumo (Tipo Faixa (kWh), Preço (R$)):
+# Residencial-  Até 500 = 0,40  / Acima de 500 = 0,65
+# Comercial - Até 1000 = 0,55 / Acima de 1000 = 0,60
+# Industrial - Até 5000 = 0,55 / Acima de 5000 = 0,60
+
+
+kwh = float(input('Quantos kwh foram consumidos? '))
+tipo = input('Qual o tipo de instalação (R, C ou I)? ')
+
+if (tipo == 'R'):
+    if kwh <= 500:
+        preco = 0.4
+    else:
+        preco = 0.65
+elif (tipo == 'C'):
+    if kwh <= 1000:
+        preco = 0.55
+    else:
+        preco = 0.6
+elif (tipo == 'I'):
+    if kwh <= 5000:
+        preco = 0.55
+    else:
+        preco = 0.6
+else:
+    print('Instalaçao inválida!')
+
+print('Total a pagar: {} ' .format(kwh * preco))
